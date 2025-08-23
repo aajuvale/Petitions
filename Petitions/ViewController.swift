77b38549cbd261a746f47a57633838e74ff8ecb1
aajuvale <ahmed.juvale@gmail.com>
2025-08-23 09:23:06 -0700

    Initial Commit

---
+++ Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petitions
+//
+//  Created by Ahmed Juvale on 8/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
