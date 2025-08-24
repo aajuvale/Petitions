b29cf61f1fecc1343a4194743f91f27e49d7503d
aajuvale <ahmed.juvale@gmail.com>
2025-08-23 17:08:32 -0700

    feat: added error messages

--- Petitions/ViewController.swift
+++ Petitions/ViewController.swift
@@ -13,13 +13,31 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
 
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
             }
         }
+
+        showError()
+    }
+
+    func showError() {
+        let ac = UIAlertController(
+            title: "Loading Error",
+            message: "There was an issue loading the data.",
+            preferredStyle: .alert
+        )
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 
     func parse(json: Data) {
